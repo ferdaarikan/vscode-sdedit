14003cb6f18a6ac19f9f50b804309a529383aa0c
ferdaarikan <ferdaarikan>
2017-08-06 13:08:24 +0100

    smaller progress ring

--- sequenceDiagramProvider.js
+++ sequenceDiagramProvider.js
@@ -31,13 +31,12 @@ class sequenceDiagramProvider{
         }
 
         .loader {
-        border: 16px solid #f3f3f3; /* Light grey */
-        border-top: 16px solid #3498db; /* Blue */
+        border: 4px solid #f3f3f3; /* Light grey */
+        border-top: 4px solid #3498db; /* Blue */
         border-radius: 50%;
         width: 40px;
         height: 40px;
-        animation: spin 2s linear infinite;
-        position: relative;
+        animation: spin 2s linear infinite;        
         }
 
         @keyframes spin {
